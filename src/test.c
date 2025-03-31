378f1e54bbb7b284cd42a2c9b4b377a3fe3bb47a
Thomas Cali <tcali@bess-f4r2s12.clusters.42paris.fr>
2025-03-31 16:12:52 +0200

    made modif on Todo.txt

--- src/test.c
+++ src/test.c
@@ -6,7 +6,7 @@
 /*   By: tcali <tcali@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 12:16:26 by tcali             #+#    #+#             */
-/*   Updated: 2025/03/31 15:15:53 by tcali            ###   ########.fr       */
+/*   Updated: 2025/03/31 15:49:24 by tcali            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -105,22 +105,6 @@ void	display_textures(t_data *data, int x, int y)
 	}
 }
 
-void	fill_with_floor(t_data *data, int x, int y)
-{
-	while (y < data->height)
-	{
-		mlx_put_image_to_window(data->mlx_ptr, data->win_ptr,
-			data->assets.xpm_floor, x, y);
-		while (x < data->width)
-		{
-			mlx_put_image_to_window(data->mlx_ptr, data->win_ptr,
-				data->assets.xpm_floor, x, y);
-				x++;
-		}
-		y++;
-	}
-}
-
 int	main(void)
 {
 	t_data	*data;
@@ -139,8 +123,7 @@ int	main(void)
 	if (!data->win_ptr)
 		return (free(data->mlx_ptr), 1);
 	load_assets(data);
-	fill_with_floor(data, x ,y);
-	//display_textures(data, x, y);
+	display_textures(data, x, y);
 	// mlx_put_image_to_window(data->mlx_ptr, data->win_ptr,
 		// data->assets.xpm_floor, x, y);
 	// Register key release hook
@@ -151,3 +134,17 @@ int	main(void)
 	mlx_loop(data->mlx_ptr);
 	return (0);
 }
+
+
+// if (all_collectables_collected && exit_count == 1)
+	// return map_valid;
+// if (on_wall)
+	// return map_invalid;
+// if (on_collectable)
+	// collectables++;
+// if (on_exit)
+	// exits++;
+// replace_current_position_with_wall;
+// if (one_of_the_four_adjacent_directions_is_possible)
+	// return map_valid;
+// return map_invalid;
