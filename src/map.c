726debe356225a128a9c75af774145d6b72ef333
Thomas Cali <tcali@paul-f3Ar6s6.clusters.42paris.fr>
2025-04-01 11:22:41 +0200

    made a list of what makes a map valid on map.c

--- src/map.c
+++ src/map.c
@@ -6,7 +6,7 @@
 /*   By: tcali <tcali@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 10:54:57 by tcali             #+#    #+#             */
-/*   Updated: 2025/04/01 11:11:30 by tcali            ###   ########.fr       */
+/*   Updated: 2025/04/01 11:22:08 by tcali            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,3 +14,14 @@
 #include "../includes/ft_printf.h"
 #include "../includes/so_long.h"
 
+/*
+First need to check type of file (must be .ber)
+Then need to check if file is not empty.
+
+If map file has the right format and is not empty>>
+	map must be rectangular
+				enclosed by walls (first and last lines + columns = 1)
+			have only one exit (E)
+			have only one start position (P)
+			have at least three collectibles (C)
+*/
